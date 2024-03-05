da8d7965e9b6d8acdde77b1c774725065ae12950
Whatcanisay824 <161446641+Whatcanisay824@users.noreply.github.com>
2024-03-05 19:00:41 +0800

    Add files via upload

---
+++ Bubble/src/Main.java
@@ -0,0 +1,12 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击间距中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) throws Exception{
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修复。
+        int[] array = {38,65,3,17,2};
+
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.BubbleSort(array);
+    }
+}
